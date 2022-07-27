cc949781e740e0212938f763a60ea7fdf0449502
dungminhnguyen257 <nguy7670@mylaurier.ca>
2022-07-27 16:12:26 -0400

    add an event listener and prevent default behaviour of the submit event, create AJAX post request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,40 +3,40 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function () {
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
 
-  const renderTweets = function (tweets) {
-    for (const tweet of tweets) {
-      createTweetElement(tweet);
-    }
-  };
+const data = [
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
+    },
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1461116232227,
+  },
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
+    },
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
+  },
+];
 
-  const createTweetElement = function (data) {
-    let $tweet = `<article class="tweet">
+const renderTweets = function (tweets) {
+  for (const tweet of tweets) {
+    createTweetElement(tweet);
+  }
+};
+
+const createTweetElement = function (data) {
+  let $tweet = `<article class="tweet">
     <div class="header">
       <div class="user">
         <img src="${data.user.avatars}" width="50" height="50">
@@ -56,8 +56,18 @@ $(document).ready(function () {
       </div>
     </footer>
   </article>`;
-    return $("#tweets-container").append($tweet);
-  };
+  return $("#tweets-container").append($tweet);
+};
 
+$(document).ready(function () {
   renderTweets(data);
+
+  $("#new-tweet").on("submit", (evt) => {
+    evt.preventDefault();
+    const param = $("#new-tweet").serialize();
+    console.log(param);
+    $.post("/tweets", param).then((data) => {
+      console.log(data);
+    });
+  });
 });
