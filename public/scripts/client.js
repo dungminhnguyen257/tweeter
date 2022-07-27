503287f631b2357f4277deb3c95e7164c8313689
dungminhnguyen257 <bryan225577@gmail.com>
2022-07-27 20:39:11 +0000

    Implement loadtweets and renderTweets functionusing AJAX to fetch (GET) data from the server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,30 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
+// const data = [
+//   {
+//     user: {
+//       name: "Newton",
+//       avatars: "https://i.imgur.com/73hZDYK.png",
+//       handle: "@SirIsaac",
+//     },
+//     content: {
+//       text: "If I have seen further it is by standing on the shoulders of giants",
+//     },
+//     created_at: 1461116232227,
+//   },
+//   {
+//     user: {
+//       name: "Descartes",
+//       avatars: "https://i.imgur.com/nlhLi3I.png",
+//       handle: "@rd",
+//     },
+//     content: {
+//       text: "Je pense , donc je suis",
+//     },
+//     created_at: 1461113959088,
+//   },
+// ];
 
 const renderTweets = function (tweets) {
   for (const tweet of tweets) {
@@ -48,7 +48,7 @@ const createTweetElement = function (data) {
     <p>${data.content.text}</p>
     </div>
     <footer>
-      <div class="time">${data.created_at}</div>
+    <div class="time">${timeago.format(data.created_at)}</div>
       <div class="icons">
         <i class="fas fa-flag"></i>
         <i class="fas fa-retweet"></i>
@@ -59,9 +59,15 @@ const createTweetElement = function (data) {
   return $("#tweets-container").append($tweet);
 };
 
-$(document).ready(function () {
-  renderTweets(data);
+const loadtweets = function () {
+  $.get("/tweets").then(function (res) {
+    renderTweets(res);
+  });
+};
 
+$(document).ready(function () {
+  // renderTweets(data);
+  loadtweets();
   $("#new-tweet").on("submit", (evt) => {
     evt.preventDefault();
     const param = $("#new-tweet").serialize();
