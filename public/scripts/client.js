1a55ec598392cd325a9ae1a4266e7b277953bc2b
dungminhnguyen257 <bryan225577@gmail.com>
2022-07-31 20:30:53 +0000

    Delete commented out lines of code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -28,15 +28,6 @@ const createTweetElement = function (tweetData) {
   return $tweet;
 };
 
-// const createLoadingEl = function () {
-//   const $tweet = `<section class="loader-wrapper">
-//         <svg class="cube" style="height:80px" version="1.1" viewBox="0 0 490.45 490.45" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
-//           <path d="m245.23 0-201.39 126.81v236.82l201.39 126.81 201.39-126.81v-236.82l-201.39-126.81zm158.24 135.1-158.24 99.643-158.24-99.643 158.24-99.643 158.24 99.643zm-329.63 27.172 156.39 98.477v184.81l-156.39-98.478v-184.81zm186.39 283.29v-184.81l156.39-98.478v184.81l-156.39 98.478z" height:120px"/>
-//         </svg>
-//       </section>`;
-//   return $tweet;
-// };
-
 //Renders tweets by looping through data base to produce dynamic tweet
 const renderTweets = function (tweetsArr) {
   for (const tweetData of tweetsArr) {
@@ -83,14 +74,16 @@ const postNewTweetOnSubmit = function () {
 
     //Request to post information to data base via AJAX request
     const param = $("#new-tweet").serialize();
-    $.post("/tweets", param).then(() => {
-      // setTimeout(() => {
-      muteErrorMessage();
-      $("#tweet-text").val("");
-      $("[name='counter']").html(maxCounter);
-      loadtweets();
-      // }, 3000);
-    });
+    $.post("/tweets", param)
+      .then(() => {
+        muteErrorMessage();
+        $("#tweet-text").val("");
+        $("[name='counter']").html(maxCounter);
+        loadtweets();
+      })
+      .catch((error) => {
+        alert(error);
+      });
   });
 };
 
