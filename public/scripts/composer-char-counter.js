5b154393e677c37a6dad1902dfb2d656ab7fd5ac
dungminhnguyen257 <bryan225577@gmail.com>
2022-07-31 20:24:45 +0000

    add comments to composer-char-counter.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
+  //Assign the name atribute to a variable
   const counter = $("[name='counter']");
 
   $("#tweet-text").on("input", function () {
     const charLeft = 140 - this.value.length;
+    //Set the text of the element as the remaining character
     counter.text(charLeft);
+    //If the remaining character is smaller than zero then change its color to red
     if (charLeft < 0) {
       counter.addClass("warning");
     } else {
